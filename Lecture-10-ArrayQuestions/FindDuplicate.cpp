994f6eb6ce60c2d458bc31c87c18c385941e9830
shashikant <pawarsk0595@gmail.com>
2022-09-11 18:31:43 +0530

    Find Duplicate

---
+++ Lecture-10-ArrayQuestions/FindDuplicate.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findDuplicate(vector<int> &arr) 
+{
+    int ans = 0;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        ans = ans ^ arr[i];
+    }
+    for (int i = 0; i < arr.size(); i++)
+    {
+        ans = ans ^ i;
+    }
+    return ans;
+}
+
+int main()
+{
+
+    vector<int> vect;
+    vect.push_back(4);
+    vect.push_back(2);
+    vect.push_back(2);
+    vect.push_back(3);
+    vect.push_back(1);
+    int ans = findDuplicate(vect);
+    cout << ans;
+}
