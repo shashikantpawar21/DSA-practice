cb1d699f5ba6fcd8f0be65156848e2f7daf401d3
shashikantpawar21 <pawarsk0595@gmail.com>
2023-12-09 18:26:13 +0530

    Lec6- Binary n Decimal

--- Lecture-6-Binary-Decimal/NegativeDecToBinary.cpp
+++ Lecture-6-Binary-Decimal/NegativeDecToBinary.cpp
@@ -5,29 +5,40 @@ using namespace std;
 int main()
 {
     int n, ans, i = 0;
-    cout << "enter a no " << endl;
+    cout << "enter a no 1 " << endl;
     cin >> n;
     // **********  UNSOLVED
     // Ignore the -ve sign
-    //  Take 1 complement
-    //  Take 2 complememt
+    //  Take 1 complement // reverse bits
+    //  Take 2 complememt // add 1
+    // Adding 1 will have carry involved
+    //   bit- 1,carry = 1 --> bit - 0, carry = 1
+    //   bit- 0,carry = 1 --> bit - 1, carry = 0
+    //   bit- 1,carry = 0 --> bit - 1, carry = 0
+    //   bit- 0,carry = 0 --> bit - 0, carry = 0
+
+    // STILL UNSOLVED WOULD TACKLE OF LOOK AT THIS WILL EXPLANATION AND VIDEOS
     n = n * -1;
-    bool reverse = false;
-    while (n != 0)
+    int carry = 1;
+    while (n != 0 && carry == 0)
     {
         int bit = (n & 1);
-        if (bit == 1 && reverse == false)
+        bit = !bit;
+        if (n == 0)
         {
-            reverse = true;
+            ans = (carry * pow(10, i)) + ans;
+            break;
         }
-        if (reverse)
+        if (carry == 1)
         {
+            carry = bit;
             bit = !bit;
         }
+
         ans = (bit * pow(10, i)) + ans;
 
         i++;
-        n = n >> 5;
+        n = n >> 1;
     }
     cout << ans;
 }
